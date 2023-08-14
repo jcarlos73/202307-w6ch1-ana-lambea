3db326eadd6111446eb11d03aa43bb70f48c1a4b
Ana Lambea <analambeap@gmail.com>
2023-08-14 23:24:15 +0200

    Add loadFilms function and filmsContextVale

--- src/store/films/context/FilmContextProvider.tsx
+++ src/store/films/context/FilmContextProvider.tsx
@@ -1,14 +1,30 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useCallback, useMemo, useState } from "react";
+import { filmsMock } from "../../../mocks/filmsMock";
 import { FilmStructure } from "../../../types";
 import FilmsContext from "./FilmsContext";
+import { FilmsContextStructure } from "./type";
 
 const FilmsContextProvider = ({
   children,
 }: PropsWithChildren): React.ReactElement => {
-  const [films] = useState<FilmStructure[]>([]);
+  const [films, setFilms] = useState<FilmStructure[]>([]);
+
+  const loadFilms = useCallback(() => {
+    setFilms(filmsMock);
+  }, []);
+
+  const filmsContextValue = useMemo(
+    (): FilmsContextStructure => ({
+      films,
+      loadFilms,
+    }),
+    [films, loadFilms],
+  );
 
   return (
-    <FilmsContext.Provider value={films}>{children}</FilmsContext.Provider>
+    <FilmsContext.Provider value={filmsContextValue}>
+      {children}
+    </FilmsContext.Provider>
   );
 };
 
