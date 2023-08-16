50219d18210bfac21cc8c5b6c9d604ecb5b62726
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-16 12:03:37 +0200

    git status

--- src/mocks/filmsMock.ts
+++ src/mocks/filmsMock.ts
@@ -1,14 +1,5 @@
 import { FilmStructure } from "../types";
 
-export const filmMock: FilmStructure = {
-  id: 1,
-  title: "Viaje a Darjeeling",
-  poster:
-    "https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/67/28/56/20350733.jpg",
-  director: "Wes Anderson",
-  year: 2007,
-};
-
 export const filmsMock: FilmStructure[] = [
   {
     id: 1,
